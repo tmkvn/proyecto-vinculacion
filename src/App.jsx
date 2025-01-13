5ba8eecf63c4fc796159e11ebf94491168a740ff
tmkvn <122834007+tmkvn@users.noreply.github.com>
2025-01-13 15:10:40 -0500

    Agregar Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="w-full h-full">
-        <iframe
-          src="/SubstractionGame/SubstractionGame.html"
-          width="100%"
-          height="500px"
-          title="Godot Game"
-          style={{
-            border: "none",
-            display: "block",
-            objectFit: "contain",
-            minHeight: "75vh",
-          }}
-        />
-      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }

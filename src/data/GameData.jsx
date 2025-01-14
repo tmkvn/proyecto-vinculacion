cfebf8e32195c779acc54ecaeba61185ad1f26fd
tmkvn <122834007+tmkvn@users.noreply.github.com>
2025-01-13 21:05:32 -0500

    Crea archivo GameData.js
    Contiene datos de juegos y mascotas

---
+++ src/data/GameData.jsx
@@ -0,0 +1,16 @@
+export const games = [
+  {
+    id: "sumas",
+    title: "Sumas",
+    icon: "➕",
+    href: "game/sumas",
+  },
+  {
+    id: "restas",
+    title: "Restas",
+    icon: "➖",
+    href: "game/restas",
+  },
+];
+
+export const mascots = ["🐱", "🐧", "🐷", "🦖", "🐑", "🐼", "🐮", "🐸"];
